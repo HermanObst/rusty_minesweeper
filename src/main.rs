44522aeaa9dfaab4a55bba372ed7a2d62e0d21d8
Herman Obst Demaestri <herman.obst@lambdaclass.com>
2022-10-14 01:36:24 -0500

    run cargo clippy pedantic

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::io::Read;
 
 fn main() -> Result<(), std::io::Error> {
     let string_file = read_file("input.txt")?;
-    let matrix_file = process_file(string_file);
-    let _matrix = count_mines(matrix_file);
+    let matrix_file = process_file(&string_file);
+    let _matrix = count_mines(&matrix_file);
     Ok(())
 }
 
@@ -16,9 +16,9 @@ fn read_file(file_path: &str) -> std::io::Result<String> {
     Ok(contents)
 }
 
-fn process_file(string_file: String) -> Vec<Vec<char>> {
+fn process_file(string_file: &str) -> Vec<Vec<char>> {
     let mut matrix_file = Vec::new();
-    for fila_str in string_file.as_str().split('\n') {
+    for fila_str in string_file.split('\n') {
         let fila = fila_str.chars().collect::<Vec<char>>();
         matrix_file.push(fila);
     }
@@ -26,12 +26,12 @@ fn process_file(string_file: String) -> Vec<Vec<char>> {
     matrix_file
 }
 
-fn count_mines(board_matrix: Vec<Vec<char>>) -> Vec<Vec<usize>> {
+fn count_mines(board_matrix: &[Vec<char>]) -> Vec<Vec<usize>> {
     let mut count_matrix = Vec::<Vec<usize>>::new();
     for (i, row) in board_matrix.iter().enumerate() {
         let mut counted_row = Vec::new();
         for (j, col) in row.iter().enumerate() {
-            counted_row.push(count_neighbour_mines(&board_matrix, i, j, col));
+            counted_row.push(count_neighbour_mines(board_matrix, i, j, *col));
         }
         count_matrix.push(counted_row);
     }
@@ -39,32 +39,29 @@ fn count_mines(board_matrix: Vec<Vec<char>>) -> Vec<Vec<usize>> {
     count_matrix
 }
 
-fn count_neighbour_mines(board_matrix: &[Vec<char>], i: usize, j: usize, col: &char) -> usize {
-    let mut count = 0_usize;
-    if *col == '*' {
-        return 0_usize;
-    } else {
-        for off_i in [-1_isize, 0, 1] {
-            for off_j in [-1_isize, 0, 1] {
-                match (
-                    usize::try_from((i as isize) + off_i),
-                    usize::try_from((j as isize) + off_j),
-                ) {
-                    (Ok(idx_i), Ok(idx_j)) => {
-                        if let Some(visited_row) = board_matrix.get(idx_i) {
-                            if let Some(char) = visited_row.get(idx_j) {
-                                {
-                                    if *char == '*' {
-                                        count += 1;
-                                    }
-                                }
+fn count_neighbour_mines(board_matrix: &[Vec<char>], i: usize, j: usize, col: char) -> usize {
+    let i = isize::try_from(i).unwrap_or(std::isize::MAX);
+    let j = isize::try_from(j).unwrap_or(std::isize::MAX);
+    let mut count = 0;
+    if col == '*' {
+        return 0;
+    }
+    for off_i in [-1_isize, 0, 1] {
+        for off_j in [-1_isize, 0, 1] {
+            if let (Ok(idx_i), Ok(idx_j)) = (usize::try_from(i + off_i), usize::try_from(j + off_j))
+            {
+                if let Some(visited_row) = board_matrix.get(idx_i) {
+                    if let Some(char) = visited_row.get(idx_j) {
+                        {
+                            if *char == '*' {
+                                count += 1;
                             }
                         }
                     }
-                    (_, _) => {}
                 }
             }
         }
     }
+
     count
 }
